20d32370cd86a2018e44e4f4984b651702f3190f
Joshua Carp <jm.carp@gmail.com>
2014-07-21 15:44:39 +0000

    Turn off emails in production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,7 +74,7 @@ Gitlab::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.delivery_method = :test
   # Defaults to:
   # # config.action_mailer.sendmail_settings = {
   # #   location: '/usr/sbin/sendmail',
